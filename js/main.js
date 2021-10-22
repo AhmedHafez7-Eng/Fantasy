ddfca5bfbf40ae5742443b00eb2c2e895fcf50eb
Ahmed Hafez <61163635+AhmedHafez7-Eng@users.noreply.github.com>
2021-10-22 23:38:36 +0200

    New Features - JS
    Scroll Indicator - Skills Bar - Counter Up Stats

--- js/main.js
+++ js/main.js
@@ -104,3 +104,112 @@ listVideo.forEach(video => {
         };
     };
 });
+
+/* -----------------
+Create Counter Up For Stats
+--------------*/
+"use strict"
+
+document.addEventListener("DOMContentLoaded", function () {
+    // You can change this class to specify which elements are going to behave as counters.
+    var elements = document.querySelectorAll(".scroll-counter")
+
+    elements.forEach(function (item) {
+        // Add new attributes to the elements with the '.scroll-counter' HTML class
+        item.counterAlreadyFired = false
+        item.counterSpeed = item.getAttribute("data-counter-time") / 45
+        item.counterTarget = +item.innerText
+        item.counterCount = 0
+        item.counterStep = item.counterTarget / item.counterSpeed
+
+        item.updateCounter = function () {
+            item.counterCount = item.counterCount + item.counterStep
+            item.innerText = Math.ceil(item.counterCount)
+
+            if (item.counterCount < item.counterTarget) {
+                setTimeout(item.updateCounter, item.counterSpeed)
+            } else {
+                item.innerText = item.counterTarget
+            }
+        }
+    })
+
+    // Function to determine if an element is visible in the web page
+    var isElementVisible = function isElementVisible(el) {
+        var scroll = window.scrollY || window.pageYOffset
+        var boundsTop = el.getBoundingClientRect().top + scroll
+        var viewport = {
+            top: scroll,
+            bottom: scroll + window.innerHeight,
+        }
+        var bounds = {
+            top: boundsTop,
+            bottom: boundsTop + el.clientHeight,
+        }
+        return (
+            (bounds.bottom >= viewport.top && bounds.bottom <= viewport.bottom) ||
+            (bounds.top <= viewport.bottom && bounds.top >= viewport.top)
+        )
+    }
+
+    // Funciton that will get fired uppon scrolling
+    var handleScroll = function handleScroll() {
+        elements.forEach(function (item, id) {
+            if (true === item.counterAlreadyFired) return
+            if (!isElementVisible(item)) return
+            item.updateCounter()
+            item.counterAlreadyFired = true
+        })
+    }
+
+    // Fire the function on scroll
+    window.addEventListener("scroll", handleScroll)
+});
+
+/* -----------------
+Start Page Scroll Indicator
+--------------*/
+window.onscroll = function () {
+    scrollProgress()
+};
+
+function scrollProgress() {
+    var currentState = document.body.scrollTop || document.documentElement.scrollTop;
+    var pageHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrollStatePercentage = (currentState / pageHeight) * 100;
+    document.querySelector(".page-scroll-indicator > .topProgress").style.width = scrollStatePercentage + "%";
+}
+
+/* -----------------
+Start Skill bar Animation
+--------------*/
+const skillSection = document.getElementById('skills');
+const progressBars = document.querySelectorAll('.skills .container .skillsItems .skill .progress .progressBar');
+
+function showProgress() {
+    progressBars.forEach(progressBar => {
+
+        const value = progressBar.dataset.progress;
+
+        progressBar.style.opacity = 1;
+        progressBar.style.width = `${value}%`;
+    });
+}
+
+function hideProgress() {
+    progressBars.forEach(p => {
+        p.style.opacity = 0;
+        p.style.width = 0;
+    });
+}
+
+window.addEventListener('scroll', () => {
+    const sectionPosition = skillSection.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 2;
+
+    if (sectionPosition < screenPosition) {
+        showProgress();
+    } else {
+        hideProgress();
+    }
+});
